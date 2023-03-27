bc1e3c9c810d57d047e5fc727581585b234bda5a
KearnsyR <rhyskearns_8@hotmail.com>
2023-03-27 01:24:24 +0100

    Display working

--- display_grid.py
+++ display_grid.py
@@ -10,11 +10,11 @@ class DisplayGrid:
         for x in range(GRID_SIZE):
             for y in range(GRID_SIZE):
                 if(grid[x][y].border_side['North']):
-                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, (y * TILE_SIZE, x * TILE_SIZE, TILE_SIZE, BORDER_WIDTH))
+                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, BORDER_WIDTH))
                 if(grid[x][y].border_side['South']):
-                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, (y * TILE_SIZE, (x + 1) * TILE_SIZE, TILE_SIZE, BORDER_WIDTH))
+                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, (x * TILE_SIZE, (y + 1) * TILE_SIZE, TILE_SIZE, BORDER_WIDTH))
                 if(grid[x][y].border_side['East']):
-                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, ((y + 1) * TILE_SIZE, x * TILE_SIZE, BORDER_WIDTH, TILE_SIZE))
+                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, ((x + 1) * TILE_SIZE, y * TILE_SIZE, BORDER_WIDTH, TILE_SIZE))
                 if(grid[x][y].border_side['West']):
-                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, (y * TILE_SIZE, x * TILE_SIZE, BORDER_WIDTH, TILE_SIZE))
+                    pygame.draw.rect(self.display_surface, BORDER_COLOUR, (x * TILE_SIZE, y * TILE_SIZE, BORDER_WIDTH, TILE_SIZE))
         pygame.display.update()
